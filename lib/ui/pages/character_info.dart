d1ad9b9aff3ac73ac22783f407d756f49be1eed3
DanielKarapetyan777 <danielkarapetyan111@gmail.com>
2022-06-30 14:10:07 +0400

    commit 3

--- lib/ui/pages/character_info.dart
+++ lib/ui/pages/character_info.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, no_logic_in_create_state, use_key_in_widget_constructors
+// ignore_for_file: prefer_typing_uninitialized_variables
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_5/ui/widgets/characters_own_page.dart';
 
 class CharacterInfo extends StatefulWidget {
   final id;
@@ -11,117 +10,93 @@ class CharacterInfo extends StatefulWidget {
   final gender;
   final image;
   const CharacterInfo({
+    Key? key,
     this.id,
     this.name,
     this.status,
     this.species,
     this.gender,
     this.image,
-  });
+  }) : super(key: key);
 
   @override
-  State<CharacterInfo> createState() => _CharacterInfoState(
-      id: id,
-      name: name,
-      species: species,
-      status: status,
-      gender: gender,
-      image: image);
+  State<CharacterInfo> createState() => _CharacterInfoState();
 }
 
 class _CharacterInfoState extends State<CharacterInfo> {
-  final id;
-  final name;
-  final status;
-  final species;
-  final gender;
-  final image;
-  _CharacterInfoState({
-    this.id,
-    this.name,
-    this.status,
-    this.species,
-    this.gender,
-    this.image,
-  });
-
   @override
   Widget build(BuildContext context) {
+    Color color = Colors.red;
+    if (widget.status.length == 5) {
+      color = Colors.green;
+    } else if (widget.status.length > 5) {
+      color = Colors.deepOrangeAccent;
+    }
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          '$name',
-          style: const TextStyle(
-              fontSize: 35, color: Color.fromARGB(255, 46, 52, 53)),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            '${widget.name}',
+            style: const TextStyle(
+                fontSize: 35, color: Color.fromARGB(255, 46, 52, 53)),
+          ),
+          elevation: 0,
+          backgroundColor: Colors.grey,
         ),
-        elevation: 0,
-        backgroundColor: Colors.grey,
-      ),
-      backgroundColor: const Color.fromARGB(255, 46, 52, 53),
-      body: CharactersOwnPage(
-          image: image,
-          name: name,
-          gender: gender,
-          status: status,
-          species: species),
-    );
+        backgroundColor: const Color.fromARGB(255, 46, 52, 53),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                height: 300,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(
+                        image: Image.network(widget.image).image,
+                        fit: BoxFit.cover)),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Name  -  ${widget.name}',
+                style: const TextStyle(
+                    fontSize: 30, color: Color.fromARGB(255, 138, 202, 204)),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Gender  -  ${widget.gender}',
+                style: const TextStyle(
+                    fontSize: 20, color: Color.fromARGB(255, 138, 202, 204)),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Species  -  ${widget.species}',
+                style: const TextStyle(
+                    fontSize: 20, color: Color.fromARGB(255, 138, 202, 204)),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Status  -  ${widget.status}',
+                style: TextStyle(fontSize: 20, color: color),
+              ),
+            ),
+            const SizedBox(
+              height: 100,
+            )
+          ],
+        ) // CharactersOwnPage(widget: widget),
+        );
   }
 }
-
-
-
-
-
-
-  // Widget build(BuildContext context) {
-  //   final id = ModalRoute.of(context)?.settings.arguments;
-  //   late Character _currentCaracter;
-  //   List<Results> _curretResults = [];
-
-  //   @override
-  //   void initState() {
-  //     if (_curretResults.isEmpty) {
-  //       context.read<CharacterBloc>().add(CharacterEvent.info(id: id));
-  //       super.initState();
-  //     }
-  //   }
-
-  //   final state = context.watch<CharacterBloc>().state;
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       centerTitle: true,
-  //       title: const Text(
-  //         '',
-  //         style:
-  //             TextStyle(fontSize: 35, color: Color.fromARGB(255, 46, 52, 53)),
-  //       ),
-  //       elevation: 0,
-  //       backgroundColor: Colors.grey,
-  //     ),
-  //     backgroundColor: const Color.fromARGB(255, 46, 52, 53),
-  //     body: state.when(
-  //       loading: () {
-  //         return Center(
-  //           child: Row(
-  //             children: const [
-  //               CircularProgressIndicator(strokeWidth: 2),
-  //               SizedBox(width: 20),
-  //               Text('Loading...')
-  //             ],
-  //           ),
-  //         );
-  //       },
-  //       loaded: (characterLoaded) {
-  //         _currentCaracter = characterLoaded;
-  //         _curretResults = _currentCaracter.results;
-  //         return _curretResults.isNotEmpty
-  //             ? CharacterList(curretResults: _curretResults)
-  //             : const SizedBox();
-  //       },
-  //       error: () => const Center(
-  //         child: Text('Nathing found...'),
-  //       ),
-  //     ),
-  //   );
-  // }
